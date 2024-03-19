404fe6a565ea30f81e5880b668e0cb334896223b
ggako <19727160+ggako@users.noreply.github.com>
2024-03-19 15:56:27 +0800

    update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,9 @@ st.markdown(
     """
     This application is built to visualize the estimated probabilities reaching a certain placement in a tournament of teams which is calculated using simulation.
     There are currently two available visualizations which can be selected at the sidebar:
-    - **Heatmap**: to see the overall scope of probability distributions
-    - **Placement**: to see only the distribution for a specific placement
+    - **Heatmap**: to see the overall landscape of probability distributions
+    - **Placement**: to see the distribution for a specific placement
+    - **Range**: to see the distribution for a specific range of placement (e.g. % Chance of getting Top 4, Bottom 8, Rank 6-10)
+    <br/>
 """
 )
