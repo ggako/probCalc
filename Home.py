9279658df3799f588f1bcba5abb6a6fa72366f84
ggako <19727160+ggako@users.noreply.github.com>
2024-03-20 14:18:03 +0800

    feat: update page files

--- Home.py
+++ Home.py
@@ -7,10 +7,11 @@ st.write("# Placement Probabality Estimator")
 st.markdown(
     """
     This application is built to visualize the estimated probabilities reaching a certain placement in a tournament of teams which is calculated using simulation.
-    There are currently two available visualizations which can be selected at the sidebar:
+    There are currently three available visualizations which can be selected at the sidebar:
     - **Heatmap**: to see the overall landscape of probability distributions
     - **Placement**: to see the distribution for a specific placement
     - **Range**: to see the distribution for a specific range of placement (e.g. % Chance of getting Top 4, Bottom 8, Rank 6-10)
-    <br/>
-"""
+    - **Floor Expected Ceiling**: to see the floor, expected range, ceiling of placements of teams after the tournament
+    - **PPG Recommender**: to see the recommended PPG in the remaining rounds to achieve a certain placement by the end of the tournament
+    """
 )
--- pages/4_Floor Expected Ceiling.py
+++ pages/4_Floor Expected Ceiling.py
@@ -8,8 +8,10 @@ st.set_page_config(page_title='Placement', page_icon='images/mountain_icon.png',
 # Title
 st.write('''
     # Placement Probabality Estimator
-    ### Range Placement (Specified Range) (e.g. Top 8, Bottom 8, Between 3-5)
+    ### Floor Expected Ceiling
     ''')
+
+st.sidebar.header('User Input')
 
 st.sidebar.header('User Input')
 
@@ -28,21 +30,12 @@ rounds = st.sidebar.number_input("Number of rounds remaining", value=1, placehol
 # Number of trials in simulation input
 numTrials = st.sidebar.number_input("Number of trials", value=100000, placeholder="Type a number...", min_value = 100000, max_value = 500000, step=100000)
 
-toLabel = st.sidebar.selectbox(
-'Include Label',
-[True, False],
-index=0
-)
-
-# Upper placement
-upper = st.sidebar.number_input("Insert upper placement (1 to 15)", value=None, placeholder="", min_value = 1, max_value = 15)
-
-# Lower placement
-lower = st.sidebar.number_input("Insert lower placement (2 to 16)", value=None, placeholder="", min_value = 2, max_value = 16)
-
 # Run Calculation
-if upper is not None and lower is not None and filenameStanding is not None:
+if st.sidebar.button('Run Simulation') and filenameStanding is not None: 
+
     teams, standings = ppe.standingRead(filenameStanding) 
     data = ppe.convertToProbResults(ppe.simulation(teams, ppe.compileData(folderName), standings, numTrials, rounds), numTrials)
-    fig, ax = ppe.visualizeProbRangeResults(ppe.probRangeResults(data, upper, lower), teams, upper, lower, addLabel=True)
-    st.pyplot(fig)
+    df = ppe.floorExpectedCeiling(data, teams)
+    fig, ax = ppe.visualizeFloorExpectedCeiling(df)
+    with st.container():
+        st.pyplot(fig)
